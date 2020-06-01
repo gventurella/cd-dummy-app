8d662ae5acdb30143c2e5264493220cf5cd71905
gventurella <66026937+gventurella@users.noreply.github.com>
2020-06-01 13:49:39 +0200

    Add files via upload

---
+++ src/main/java/com/ca/cdd/dummy/lib/AnonymousClass.java
@@ -0,0 +1,47 @@
+package com.ca.cdd.dummy.lib;
+
+public class AnonymousClass extends Library implements InterfaceClass {
+  
+  
+  private static String NAME = "AnonymousClass";
+  InterfaceClass anonymusMethos = new InterfaceClass() {
+        @Override
+        public Integer useAbstractInInterface() {
+            return null;
+         
+        }
+   
+        @Override
+        public String interfaceStingMethod() {
+            return "Returned From Anonymous Class ";
+        }
+    };
+
+    @Override
+    public Integer useAbstractInInterface() {
+        return anonymusMethos.useAbstractInInterface();
+    }
+
+    @Override
+    public String interfaceStingMethod() {
+        return anonymusMethos.interfaceStingMethod();
+    }
+    
+    @Override
+    public String getName() {
+        return NAME;
+    }
+}
+
+
+
+// 96c07880-3b65-489a-bfab-7de4c31ec704
+
+// DummyApp
+// df0ebcac-d4e6-40fd-b853-05761019c438
+
+// DummyApp
+// DummyApp
+// DummyApp
+// DummyApp
+// DummyApp
